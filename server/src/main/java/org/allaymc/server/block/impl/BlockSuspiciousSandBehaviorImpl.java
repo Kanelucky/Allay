59d4007e322a0acf2b0add59133c81e1f7e4c501
Kanelucky <tuankietdoan46@gmail.com>
2026-08-31 18:30:01 +0700

    feat(block): impl suspicious sand and suspicious gravel (not include brush) (#1844)

--- server/src/main/java/org/allaymc/server/block/impl/BlockSuspiciousSandBehaviorImpl.java
+++ server/src/main/java/org/allaymc/server/block/impl/BlockSuspiciousSandBehaviorImpl.java
@@ -1,5 +1,7 @@
 package org.allaymc.server.block.impl;
 
+import lombok.experimental.Delegate;
+import org.allaymc.api.block.component.BlockFallableComponent;
 import org.allaymc.api.block.interfaces.BlockSuspiciousSandBehavior;
 import org.allaymc.api.component.Component;
 import org.allaymc.server.component.ComponentProvider;
@@ -7,6 +9,9 @@ import org.allaymc.server.component.ComponentProvider;
 import java.util.List;
 
 public class BlockSuspiciousSandBehaviorImpl extends BlockBehaviorImpl implements BlockSuspiciousSandBehavior {
+    @Delegate
+    private BlockFallableComponent fallableComponent;
+
     public BlockSuspiciousSandBehaviorImpl(
             List<ComponentProvider<? extends Component>> componentProviders) {
         super(componentProviders);
