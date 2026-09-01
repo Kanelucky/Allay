f9aa23111e86cd2197f078caccfb19c4808d471b
Kanelucky <tuankietdoan46@gmail.com>
2026-09-01 13:14:19 +0700

    feat(event): PlayerDeathEvent

---
+++ api/src/main/java/org/allaymc/api/eventbus/event/player/PlayerDeathEvent.java
@@ -0,0 +1,60 @@
+package org.allaymc.api.eventbus.event.player;
+
+import org.allaymc.api.entity.interfaces.EntityPlayer;
+import org.allaymc.api.eventbus.event.entity.EntityDieEvent;
+import org.allaymc.api.utils.tuple.Pair;
+
+/**
+ * Called when a player dies.
+ *
+ * @author Kanelucky
+ */
+public class PlayerDeathEvent extends EntityDieEvent {
+
+    private Pair<String, String[]> deathInfo;
+
+    public PlayerDeathEvent(EntityPlayer player, Pair<String, String[]> deathInfo) {
+        super(player);
+        this.deathInfo = deathInfo;
+    }
+
+    @Override
+    public EntityPlayer getEntity() {
+        return (EntityPlayer) super.getEntity();
+    }
+
+    /**
+     * Gets the complete death information.
+     */
+    public Pair<String, String[]> getDeathInfo() {
+        return deathInfo;
+    }
+
+    /**
+     * Sets the complete death information.
+     */
+    public void setDeathInfo(Pair<String, String[]> deathInfo) {
+        this.deathInfo = deathInfo;
+    }
+
+    /**
+     * Gets the death message translation key.
+     */
+    public String getDeathMessage() {
+        return deathInfo.left();
+    }
+
+    /**
+     * Sets the death message.
+     */
+    public void setDeathMessage(String deathMessage) {
+        this.deathInfo = new Pair<>(deathMessage, new String[0]);
+    }
+
+    /**
+     * Sets the death message translation key and formatting arguments.
+     */
+    public void setDeathMessage(String translationKey, String... arguments) {
+        this.deathInfo = new Pair<>(translationKey, arguments);
+    }
+}
