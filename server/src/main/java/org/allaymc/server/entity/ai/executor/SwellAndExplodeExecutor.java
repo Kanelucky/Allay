d6b7da75ec1561e9f1e5be474138371021d848db
Kanelucky <tuankietdoan46@gmail.com>
2026-09-06 14:01:08 +0700

    feat: impl creeper (#12)

---
+++ server/src/main/java/org/allaymc/server/entity/ai/executor/SwellAndExplodeExecutor.java
@@ -0,0 +1,181 @@
+package org.allaymc.server.entity.ai.executor;
+
+import org.allaymc.api.entity.Entity;
+import org.allaymc.api.entity.ai.behavior.BehaviorExecutor;
+import org.allaymc.api.entity.ai.memory.MemoryType;
+import org.allaymc.api.entity.ai.memory.MemoryTypes;
+import org.allaymc.api.entity.component.EntityCreeperBaseComponent;
+import org.allaymc.api.entity.data.EntityAnimation;
+import org.allaymc.api.entity.interfaces.EntityIntelligent;
+import org.allaymc.api.entity.interfaces.EntityLiving;
+import org.allaymc.api.eventbus.event.entity.EntityExplodeEvent;
+import org.allaymc.api.player.GameMode;
+import org.allaymc.api.world.explosion.Explosion;
+import org.allaymc.api.world.sound.CustomSound;
+import org.allaymc.api.world.sound.SoundNames;
+import org.joml.Vector3d;
+
+/**
+ * Based on the original implementation from
+ * <a href="https://github.com/GearsMC/Allay/blob/master/server/src/main/java/org/allaymc/server/entity/ai/executor/SwellAndExplodeExecutor.java">GearsMC/Allay</a>.
+ *
+ * @author GearMC | Kanelucky
+ */
+public class SwellAndExplodeExecutor implements BehaviorExecutor {
+
+    protected final MemoryType<Long> targetIdMemory;
+    protected final float speed;
+    protected final double maxSenseRangeSquared;
+    protected final double fuseRangeSquared;
+    protected final boolean clearTargetAfterLose;
+    protected final int fuseTime;
+    protected final float explosionSize;
+
+    protected int fuseTick;
+    protected Vector3d lastTargetPos;
+
+    public SwellAndExplodeExecutor(
+            MemoryType<Long> targetIdMemory,
+            float speed,
+            double maxSenseRange,
+            double fuseRange,
+            boolean clearTargetAfterLose,
+            int fuseTime,
+            float explosionSize) {
+        this.targetIdMemory = targetIdMemory;
+        this.speed = speed;
+        this.maxSenseRangeSquared = maxSenseRange * maxSenseRange;
+        this.fuseRangeSquared = fuseRange * fuseRange;
+        this.clearTargetAfterLose = clearTargetAfterLose;
+        this.fuseTime = fuseTime;
+        this.explosionSize = explosionSize;
+    }
+
+    @Override
+    public void onStart(EntityIntelligent entity) {
+        fuseTick = 0;
+        lastTargetPos = null;
+        entity.setMovementSpeed(speed);
+        entity.setPitchEnabled(true);
+    }
+
+    @Override
+    public boolean execute(EntityIntelligent entity) {
+        var targetId = entity.getMemoryStorage().get(targetIdMemory);
+        if (targetId == null) {
+            return false;
+        }
+
+        var target = entity.getDimension().getEntityManager().getEntity(targetId);
+        if (!(target instanceof EntityLiving) || !isTargetValid(target)) {
+            return false;
+        }
+
+        var targetLoc = target.getLocation();
+        var distanceSquared = entity.getLocation().distanceSquared(targetLoc);
+        if (distanceSquared > maxSenseRangeSquared) {
+            return false;
+        }
+
+        if (!entity.isPitchEnabled()) {
+            entity.setPitchEnabled(true);
+        }
+        if (entity.getMovementSpeed() != speed) {
+            entity.setMovementSpeed(speed);
+        }
+
+        EntityControlHelper.setLookTarget(
+                entity, new Vector3d(targetLoc.x(), targetLoc.y() + target.getEyeHeight(), targetLoc.z()));
+
+        if (distanceSquared > fuseRangeSquared) {
+            fuseTick = 0;
+            setSwelling(entity, false);
+
+            var chaseTarget = new Vector3d(targetLoc.x(), targetLoc.y(), targetLoc.z());
+            entity.setMoveTarget(chaseTarget);
+            if (lastTargetPos == null || isInDifferentBlock(lastTargetPos, chaseTarget)) {
+                entity.getBehaviorGroup().setRouteUpdateRequired(true);
+            }
+            lastTargetPos = chaseTarget;
+            return true;
+        }
+
+        EntityControlHelper.removeRouteTarget(entity);
+        lastTargetPos = null;
+
+        if (fuseTick == 0) {
+            entity.getDimension().addSound(entity.getLocation(), new CustomSound(SoundNames.RANDOM_FUSE));
+            setSwelling(entity, true);
+        }
+
+        fuseTick++;
+        if (fuseTick < fuseTime) {
+            return true;
+        }
+
+        explode(entity);
+        return false;
+    }
+
+    @Override
+    public void onStop(EntityIntelligent entity) {
+        EntityControlHelper.removeRouteTarget(entity);
+        EntityControlHelper.removeLookTarget(entity);
+        entity.setPitchEnabled(false);
+        entity.setMovementSpeed(MemoryTypes.MOVEMENT_SPEED.defaultData().get());
+        lastTargetPos = null;
+        fuseTick = 0;
+        setSwelling(entity, false);
+        if (clearTargetAfterLose) {
+            entity.getMemoryStorage().clear(targetIdMemory);
+        }
+    }
+
+    @Override
+    public void onInterrupt(EntityIntelligent entity) {
+        onStop(entity);
+    }
+
+    protected void explode(EntityIntelligent entity) {
+        var explosion = new Explosion(explosionSize);
+        explosion.setDestroyBlocks(true);
+        explosion.setSpawnFire(false);
+        explosion.setEntity(entity);
+
+        var event = new EntityExplodeEvent(entity, explosion);
+        if (!event.call()) {
+            fuseTick = 0;
+            setSwelling(entity, false);
+            return;
+        }
+
+        var location = entity.getLocation();
+        entity.remove();
+        explosion.explode(entity.getDimension(), location.x(), location.y(), location.z());
+    }
+
+    protected void setSwelling(EntityIntelligent entity, boolean swelling) {
+        if (entity instanceof EntityCreeperBaseComponent creeper) {
+            creeper.setSwelling(swelling);
+        }
+    }
+
+    protected boolean isInDifferentBlock(Vector3d oldTargetPos, Vector3d newTargetPos) {
+        return Math.floor(oldTargetPos.x()) != Math.floor(newTargetPos.x())
+                || Math.floor(oldTargetPos.y()) != Math.floor(newTargetPos.y())
+                || Math.floor(oldTargetPos.z()) != Math.floor(newTargetPos.z());
+    }
+
+    protected boolean isTargetValid(Entity target) {
+        if (!target.isAlive()) {
+            return false;
+        }
+
+        if (target.isPlayer()) {
+            var gameMode = target.asPlayer().getGameMode();
+            return gameMode == GameMode.SURVIVAL || gameMode == GameMode.ADVENTURE;
+        }
+
+        return true;
+    }
+}
